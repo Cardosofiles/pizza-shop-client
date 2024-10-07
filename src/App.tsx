f4e94c3ec232b466bd0c21ebe90e9e472fe55212
João Batista <cardosofiles@outlook.com>
2024-10-07 20:55:16 -0300

    applying light and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 
-import { router } from "./routes";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { router } from "@/routes";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Controle Financeiro" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="dashboard-financial" defaultTheme="dark">
+        <Helmet titleTemplate="%s | Controle Financeiro" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
---
+++ src/components/account-menu.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Building, ChevronDown, LogOut } from "lucide-react";
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          Dashboard Financial
+          <ChevronDown className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span className="">João Batista</span>
+          <span className="text-sm font-normal text-muted-foreground">
+            cardosofiles@outlook.com
+          </span>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Building className="w-4 h-4 mr-2" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
