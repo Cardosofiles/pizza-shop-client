1c9256155c8db9315ac7cf13f5ac198dd1eefbaa
João Batista <cardosofiles@outlook.com>
2024-10-07 21:52:18 -0300

    adding UI table

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { AppLayout } from "@/routes/_layouts/app";
 import { AuthLayout } from "@/routes/_layouts/auth";
 import { Dashboard } from "@/routes/app/dashboard";
+import { Orders } from "@/routes/app/orders/orders";
 import { SingIn } from "@/routes/auth/sing-in";
 import { SingUp } from "@/routes/auth/sing-up";
 
@@ -10,7 +11,10 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
   {
     path: "/",
